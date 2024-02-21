fe3c800f26f28083cf4b467dc5b36117f87b5f55
Kingwizar <151655786+Kingwizar@users.noreply.github.com>
2024-02-21 15:01:05 +0100

    azure_avec_2_arduinos

---
+++ azure_avec_2_arduinos/uno_wifi/configure.h
@@ -0,0 +1,5 @@
+static const char iotConnStr[] = "HostName=arduino2.azure-devices.net;DeviceId=testarduino2;SharedAccessKey=IivzReGPpwVFNVJKV/3MCtS6UF20zORbqAIoTFjGpmg=";
+
+// Wi-Fi information
+static const char wifi_ssid[] = "MobiArchitects Work Space";
+static const char wifi_password[] = "WorkSpace@2023$";
